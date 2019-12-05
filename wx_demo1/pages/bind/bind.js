2ede8c4c3d479a8659eae8510db351092fd38b79
fzk <455983827@qq.com>
2019-12-05 18:06:34 +0800

    数据绑定

---
+++ wx_demo1/pages/bind/bind.js
@@ -0,0 +1,72 @@
+// pages/bind/bind.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name:'fanzhikang',
+    flag:true,
+    info:[90,80,12],
+    infoList:[
+      {className:'17软件2班',name:'Mr.fan'},
+      {className:'14ruan',name:'fan'}
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
