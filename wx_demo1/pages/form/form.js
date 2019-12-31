c72759da4c607116c26451742ab817b3be906d76
fanzhikang <455983827@qq.com>
2019-12-31 14:04:49 +0800

    form 组件初试

--- wx_demo1/pages/form/form.js
+++ wx_demo1/pages/form/form.js
@@ -5,7 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    index:0,
+    multiIndex:[0,0,0],
+    country:['美国','中国','日本'],
+    multiArray:[
+      ['中国','美国','日本'],
+      ['北京','河南'],
+      ['周口市','南阳市']
+    ],
+    time:'9:00',
+    slidervalue:0
+  },
+  //触发改变时间
+  bindchangeTime (e) {
+    console.log(e);
+  },
+  bindMultiPicker (e) {
+    // console.log(e);
+    this.setData({
+      multiIndex:e.detail.value
+    });
+  },
+  // 当列选择器发生改变时
+  bindMultiPickerColumn (e) {
+    console.log(e);
+  },
+  // 当单列选择器发生改变时
+  bindPickerChange (e) {
+    // console.log(e.detail.value);
+    this.setData({
+      index:e.detail.value
+    });
   },
   handleSubmit (e){
     console.log(e);
@@ -13,6 +43,36 @@ Page({
   handleReset (e){
     console.log(e);
   },
+  bindchangeTime (e) {
+    console.log(e);
+    this.setData({
+      time:e.detail.value
+    });
+  },
+  bindchangedate (e) {
+    this.setData({
+      date:e.detail.value
+    });
+  },
+  bindchangeregion (e) {
+    this.setData({
+      region:e.detail.value
+    });
+  },
+  bindchangeslider (e){
+    console.log(e);
+    this.setData({
+      slidervalue:e.detail.value
+    });
+    console.log(this.data.slidervalue);
+  },
+  bindchangeswitch1 (e){
+    console.log(e);
+  },
+  bindchangeswitch2 (e){
+    console.log(e);
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
