eda1a3e005c3de3b7ac17e6b1215ff63b4643a88
fanzhikang <455983827@qq.com>
2020-01-08 15:53:51 +0800

    appMusic2

--- wx_appMusic/pages/myMusic/index.js
+++ wx_appMusic/pages/myMusic/index.js
@@ -27,7 +27,65 @@ const _songList = [
   }
 ];
 const _items = ['播放列表','歌曲',
-'专辑','演唱者']
+'专辑','演唱者'];
+const _songsList = [
+  {
+    coverImgUrl:'/image/photo.jpg',
+    singer:'蔡雅琴',
+    name:'红色高跟鞋'
+  },
+  {
+    coverImgUrl: '/image/photo.jpg',
+    singer: '蔡雅琴',
+    name: '红色高跟鞋'
+  },
+  {
+    coverImgUrl: '/image/photo.jpg',
+    singer: '蔡雅琴',
+    name: '红色高跟鞋'
+  },
+  {
+    coverImgUrl: '/image/photo.jpg',
+    singer: '蔡雅琴',
+    name: '红色高跟鞋'
+  }
+];
+const _albumList = [
+  {
+    image:'/image/photo.jpg',
+    name:'穿越火线',
+    singer:'谢霆锋'
+  },
+  {
+    image: '/image/photo.jpg',
+    name: '穿越火线',
+    singer: '谢霆锋'
+  },
+  {
+    image: '/image/photo.jpg',
+    name: '穿越火线',
+    singer: '谢霆锋'
+  },
+  {
+    image: '/image/photo.jpg',
+    name: '穿越火线',
+    singer: '谢霆锋'
+  }
+];
+const _authorList = [
+  {
+    image:'/image/photo.jpg',
+    singer:'一玟'
+  },
+  {
+    image: '/image/photo.jpg',
+    singer: '一玟'
+  },
+  {
+    image: '/image/photo.jpg',
+    singer: '一玟'
+  }
+];
 
 Page({
 
@@ -37,15 +95,68 @@ Page({
   data: {
     songList:_songList,
     musicGroupName:_items[0],
-    acionSheetHidden:true,
-    actionSheetItems:_items
+    actionSheetHidden:true,
+    actionSheetItems:_items,
+    listTemplateName:'music-play-list',
+    templateData:'',
+    playing:false,
+    playBar:{
+      coverImgUrl:'/image/photo.jpg',
+      name:'最甜情歌'
+    }
   },
   /**
    * 列表选择
    */
   actionSheetTap () {
     this.setData({
-      acionSheetHidden:!this.data.actionSheetHidden
+      actionSheetHidden:!this.data.actionSheetHidden
+    });
+  },
+  /**
+   * 显示隐藏
+   */
+  actionSheetChange () {
+    this.setData({
+      actionSheetHidden: !this.data.actionSheetHidden 
+    });
+  },
+  /**
+   * item的单击
+   */
+  bindItemTap (e) {
+    //单击链接的区域
+    let _listTemplateName = '';
+    let _templateData = '';
+    //获知现在所点击的菜单
+    // console.log(e.currentTarget.dataset.sheetitem);
+    let sheetItem = e.currentTarget.dataset.sheetitem;
+    //判断应该打开哪个模板的名字
+    switch (sheetItem) {
+      case '播放列表':
+        _listTemplateName = 'music-play-list';
+       
+        break;
+      case '歌曲' :
+        _listTemplateName = 'songs-list';
+        _templateData = _songsList;
+        break;
+      case '专辑' : 
+        _listTemplateName = 'album-list';
+        _templateData = _albumList;
+        break;
+      case '演唱者' : 
+        _listTemplateName = 'author-list';
+        _templateData = _authorList;
+        break;
+    }
+
+
+    //显示或者隐藏
+    this.setData({
+      listTemplateName: _listTemplateName,
+      templateData: _templateData,
+      actionSheetHidden: !this.data.actionSheetHidden
     });
   },
   /**
