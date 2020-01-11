6c10664db9b8c637c5bf6f120bed777c166faf50
fanzhikang <455983827@qq.com>
2020-01-11 17:12:37 +0800

    拨打电话and电量监控

---
+++ wx_demo2/pages/battery/battery.js
@@ -0,0 +1,79 @@
+// pages/battery/battery.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    batteryInfo:null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let that = this;
+    let res = wx.getBatteryInfoSync();
+    console.log(res);
+    this.setData({
+      batteryInfo: res
+    });
+    // wx.getBatteryInfo({
+    //   success: function (res) {
+    //     that.setData({
+    //       batteryInfo:res
+    //     });
+    //     console.log(res);
+    //   }
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
