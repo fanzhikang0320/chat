d4e2e5671918c12d38d769f6edd0c9d69e28c58d
fanzhikang <455983827@qq.com>
2020-01-27 14:01:00 +0800

    抽象节点

---
+++ wx_abstractNode/components/selected-group/selected-group.js
@@ -0,0 +1,24 @@
+// components/selected-group/selected-group.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    lables: [1,3,4],
+    selected: [false,false,false]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
